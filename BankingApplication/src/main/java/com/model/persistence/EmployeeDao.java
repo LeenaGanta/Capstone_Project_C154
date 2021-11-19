a5aa2d36e7b5fba1fe3b1d793738933cde006fe9
vaibhavggupta <94508690+vaibhavggupta@users.noreply.github.com>
2021-11-19 11:25:34 +0530

    Update EmployeeDao.java

--- BankingApplication/src/main/java/com/model/persistence/EmployeeDao.java
+++ BankingApplication/src/main/java/com/model/persistence/EmployeeDao.java
@@ -7,5 +7,6 @@ import com.beans.Employee;
 
 @Repository
 public interface EmployeeDao extends JpaRepository<Employee,Integer> {
-
+@Query("From Employee where empId=:empid and password=:password")
+	Employee validate(@Param("empid") int empid,@Param("password") String password);
 }
