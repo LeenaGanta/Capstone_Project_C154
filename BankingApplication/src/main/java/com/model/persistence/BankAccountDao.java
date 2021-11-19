ce2057321f0f373efa636e20f1f18b870c1183eb
Lahari Bowrampet <66938243+lahari10@users.noreply.github.com>
2021-11-19 10:17:25 +0530

    Add files via upload

--- BankingApplication/src/main/java/com/model/persistence/BankAccountDao.java
+++ BankingApplication/src/main/java/com/model/persistence/BankAccountDao.java
@@ -1,11 +1,17 @@
 package com.model.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.beans.BankAccount;
+import com.beans.User;
 
 @Repository
 public interface BankAccountDao extends JpaRepository<BankAccount,Integer> {
+
+	@Query("From BankAccount where balance=:balance")
+	BankAccount getAccountBalance(@Param("accNo") User accNo);
 
 }
