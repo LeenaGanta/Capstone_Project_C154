8306ef73c310226288cb866e78fe2bd3073274b9
Mothe Vishnu vardhan reddy <91151203+Vishnu-585@users.noreply.github.com>
2021-11-19 10:05:50 +0530

    Add files via upload

--- BankingApplication/src/main/java/com/model/persistence/TransactionDao.java
+++ BankingApplication/src/main/java/com/model/persistence/TransactionDao.java
@@ -1,11 +1,18 @@
 package com.model.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.beans.Transaction;
 
 @Repository
 public interface TransactionDao extends JpaRepository<Transaction, Long>{
+
+	@Query("select transaction from Transaction transaction where txnId=:id")
+	List<Transaction> getTransactionsById(@Param("id")long accNO);
 
 }
