c6b6789368c8cf4e170f5b3df870a60c764939b0
Lahari Bowrampet <66938243+lahari10@users.noreply.github.com>
2021-11-21 18:34:05 +0530

    Add files via upload

--- BankingApplication-ConsumerProject/src/main/java/com/model/service/AdminServiceImpl.java
+++ BankingApplication-ConsumerProject/src/main/java/com/model/service/AdminServiceImpl.java
@@ -1,30 +1,41 @@
 package com.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.beans.Admin;
 import com.beans.EmployeeList;
+import com.beans.User;
 import com.beans.UserList;
 
-@Service
+@Service("adminService")
 public class AdminServiceImpl implements AdminService {
+	
+	@Autowired
+	private RestTemplate RestTemplate;
+	
+	@Override
+	public boolean validateAdmin(String mailId,String password) {
+		ResponseEntity<Admin> admin=RestTemplate.getForEntity("http://localhost:8084/employees/"+mailId+"/"+password,Admin.class);
+		if(admin.getBody()!=null)
+			return true;
+		else
+			return false;
+	}
 
 	@Override
 	public UserList getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		ResponseEntity<UserList> user=RestTemplate.getForEntity("http://localhost:8080/users/", UserList.class);
+		return user.getBody();
+		}	
 
 	@Override
 	public EmployeeList getAllEmployees() {
-		// TODO Auto-generated method stub
-		return null;
+		ResponseEntity<EmployeeList> employee=RestTemplate.getForEntity("http://localhost:8080/employees/", EmployeeList.class);
+		return employee.getBody();
 	}
 
-	@Override
-	public Admin validateAdmin(String mailId, String password) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 }
