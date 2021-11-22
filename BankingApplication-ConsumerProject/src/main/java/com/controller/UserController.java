ea919a6ac1723895ca27d0bb40ec0af832706664
Mothe Vishnu vardhan reddy <91151203+Vishnu-585@users.noreply.github.com>
2021-11-22 11:52:30 +0530

    Add files via upload

--- BankingApplication-ConsumerProject/src/main/java/com/controller/UserController.java
+++ BankingApplication-ConsumerProject/src/main/java/com/controller/UserController.java
@@ -17,62 +17,71 @@ import com.model.service.UserService;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	TransferService transferService;
-	
+
 	@Autowired
 	TransactionService transactionService;
-	
+
 	@RequestMapping("/newUser")
 	public ModelAndView showLoginPage() {
-		return new ModelAndView("UserRegistration","user",new User());
+		return new ModelAndView("UserRegistration", "user", new User());
 //		"command",new User()
 	}
-	
+
 	@RequestMapping("/UserRegister")
-	public ModelAndView registerUserController(@ModelAttribute  User user) {
+	public ModelAndView registerUserController(@ModelAttribute User user) {
 		System.out.println(user);
-		User user1=userService.registerUser(user);
-		return new ModelAndView("UserRegistration","user",user1);
+		User user1 = userService.registerUser(user);
+		if(user1!=null)
+		return new ModelAndView("UserRegistration", "message", "Your account number is-> "+user1.getAccNo());
+		
+		return new ModelAndView("UserRegistration", "message", "Sorry registration failed Try again");
 	}
-	
+
 	@RequestMapping("/updateDetails")
 	public ModelAndView UpdateUserController(HttpSession session) {
-		User user=(User) session.getAttribute("user");
+		User user = (User) session.getAttribute("user");
 		System.out.println(user);
-		User user1=userService.updateUser(user);
-		return new ModelAndView("UserUpdation","user",user1);
+		User user1 = userService.updateUser(user);
+		return new ModelAndView("UserUpdation", "user", user1);
 	}
-	
+
 	@RequestMapping("/UserUpdateDb")
-	public ModelAndView UpdateUserControllerDB(@ModelAttribute  User user,HttpSession session) {
-		User user2=(User) session.getAttribute("user");
+	public ModelAndView UpdateUserControllerDB(@ModelAttribute User user, HttpSession session) {
+		User user2 = (User) session.getAttribute("user");
+		if (!user2.getPassword().equals(user.getPassword()))
+			return new ModelAndView("UserUpdation", "message", "Password Didnt Match");
 		user.setAccNo(user2.getAccNo());
 		System.out.println(user);
-		User user1=userService.updateUser(user);
-		session.setAttribute("user", user);
-		return new ModelAndView("UserUpdation","message","Details updated sucessfully");
+		User user1 = userService.updateUser(user);
+		session.setAttribute("user", user1);
+		return new ModelAndView("UserUpdation", "message", "Details updated sucessfully");
 	}
-	
+
 	@RequestMapping("/displayTransfers")
-	public ModelAndView displayingTransfers(HttpSession session)
-	{
-		User user=(User) session.getAttribute("user");
-		TransferList transferList=transferService.getByAccount(user.getAccNo());
-		
-		return new ModelAndView("TransfersDisplay","transferList",transferList.getTransfer());
+	public ModelAndView displayingTransfers(HttpSession session) {
+		User user = (User) session.getAttribute("user");
+		TransferList transferList = transferService.getByAccount(user.getAccNo());
+		if(transferList.getTransfer().size()==0)
+			return new ModelAndView("TransfersDisplay", "message", "No Transfers to Display");
+
+		return new ModelAndView("TransfersDisplay", "transferList", transferList.getTransfer());
 	}
+
 //	TransactionsDisplay
 	@RequestMapping("/displayTransactions")
-	public ModelAndView displayTransactions(HttpSession session)
-	{
-		User user=(User) session.getAttribute("user");
-		TransactionList transactionList=transactionService.getAllTransactionsByAccNo(user.getAccNo());
-		return new ModelAndView("TransactionsDisplay","transactionList",transactionList.getTransactions());
+	public ModelAndView displayTransactions(HttpSession session) {
+		User user = (User) session.getAttribute("user");
+		TransactionList transactionList = transactionService.getAllTransactionsByAccNo(user.getAccNo());
+		
+		if(transactionList.getTransactions().size()==0)
+			return new ModelAndView("TransactionsDisplay", "message", "No transactions To display");
+		return new ModelAndView("TransactionsDisplay", "transactionList", transactionList.getTransactions());
 	}
 
 }
